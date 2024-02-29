91eade2b5d8d32b7cc12c70cc484122cddb73b76
mutaqorrobin <ainmutaqorrobin@gmail.com>
2024-02-29 21:28:35 +0800

    code 9:28 P.M

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs-compat';
+import { filter } from 'rxjs-compat/operator/filter';
+import { map } from 'rxjs-compat/operator/map';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
-  ngOnInit() {}
+  private subscription: Subscription;
+  ngOnInit() {
+    // this.subscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    const customInterval = Observable.create((observer) => {
+      let count = 0;
+
+      setInterval(() => {
+        observer.next(count);
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        } else if (count === 2) {
+          observer.complete();
+        }
+        count++;
+      }, 1000);
+    });
+    this.subscription = customInterval.subscribe(
+      (data) => console.log(data),
+      (error) => {
+        console.log(error);
+        alert(error);
+      },
+      () => console.log(`Completed`)
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
